6e3c02cdf60378e6649c1088bdd511d77c15084b
AhmadOmar <ahmadomarns@gmail.com>
2023-09-10 01:01:31 +0300

    Centet Section Main Content

---
+++ src/components/Main Content/WhoToFollow/WhoToFollow.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WhoToFollow = () => {
+    return (
+        <div className='whot-to-follow'>
+            <div className='container'>
+                
+
+            </div>
+        </div>
+    )
+}
+
+export default WhoToFollow

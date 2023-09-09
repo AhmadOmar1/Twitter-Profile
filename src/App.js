e7bf1014625dafab3d6c92938330c755757a579f
AhmadOmar <ahmadomarns@gmail.com>
2023-09-10 01:02:12 +0300

    The App and pakage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import NavigationList from './components/Navigation/NavigationList/NavigationList'
+import YouMightLike from './components/Right Sidebar/YouMightLike/YouMightLike';
+import MainLayout from './pages/MainLayout/MainLayout';
 function App() {
   return (
     <div className="App">
-      
+     <MainLayout />
+        
     </div>
   );
 }

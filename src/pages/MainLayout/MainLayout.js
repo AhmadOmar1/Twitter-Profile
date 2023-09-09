b160f29f7fa21e7e8a4858e840f169a3204fc7bb
AhmadOmar <ahmadomarns@gmail.com>
2023-09-10 01:01:55 +0300

    MainLayout containing all the view sections

---
+++ src/pages/MainLayout/MainLayout.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import  RightSidebar from '../../components/Right Sidebar/RightSidebar'
+import MainContent from '../../components/Main Content/MainContent';
+import Navigation from '../../components/Navigation/NavigationList/NavigationList'
+import './MainLayout.css'
+
+function MainLayout() {
+  return (
+    <div className="main-layout">
+      <Navigation />
+      <MainContent />
+      <RightSidebar />
+    </div>
+  );
+}
+
+export default MainLayout;
